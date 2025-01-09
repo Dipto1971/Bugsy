56b38346f8098d0b3de800ea99caa065b93e525a
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2025-01-09 16:04:25 +0600

    refactor NavBar component and add Issues page

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,19 +1,46 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import c_Names from "classnames";
+
+// In the classnaame object,
+// we can pass the classnames we want to render as keys and the condition as values.
+
 const NavBar = () => {
+  const currentPath = usePathname();
+  // We need to convert this component to a Client-Side Component,
+  // Because the usePathname hook is not supported on the server. This is a Browser API.
+  console.log(currentPath);
+
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues", label: "Issues" },
+    { href: "/contact", label: "Contact" },
+  ];
+
   return (
-    <nav className="flex space-x-4">
-      <Link href="/"> Logo</Link>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        {" "}
+        <AiFillBug />
+      </Link>
       <ul className="flex space-x-4">
-        <li>
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={c_Names({
+              "text-zinc-900": currentPath === link.href,
+              "text-zinc-500": currentPath !== link.href,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );

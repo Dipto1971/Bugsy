5b090bf6c569995bef0357e9f3d60d6080758d4e
Mahir Faysal Dipto <86009555+Dipto1971@users.noreply.github.com>
2025-04-17 13:06:56 +0600

    refactor: update issue validation schema; enhance error handling and response format

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -2,29 +2,31 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import prisma from "@/prisma/client";
 
-const createIssueSchema = z.object({
-  title: z.string().min(5).max(255),
-  description: z.string().min(1),
+const validIssueSchema = z.object({
+  title: z.string().min(5).max(200),
+  description: z.string().min(10).max(1000),
 });
 
 export async function POST(request: NextRequest) {
-  // request object of type NextRequest
-  // This function handles the POST request to create a new issue
   const body = await request.json();
-  // Parse the request body as JSON
-  // The body should contain the title and description of the issue
-  const validation = createIssueSchema.safeParse(body); // Validate the incoming data
+  const validation = validIssueSchema.safeParse(body);
 
   if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400 }); //Client sent invalid data. Error: Bad Request,
+    return NextResponse.json(validation.error.format(), {
+      status: 400,
+      statusText: "Bad Request",
+    });
   }
+
   const newIssue = await prisma.issue.create({
     data: {
       title: body.title,
       description: body.description,
     },
   });
+
   return NextResponse.json(newIssue, {
-    status: 201, //Created
+    status: 201,
+    statusText: "Created",
   });
 }

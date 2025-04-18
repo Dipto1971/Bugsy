c66301a305cb740a952c41092ed622a8b7c51eb5
Mahir Faysal Dipto <86009555+Dipto1971@users.noreply.github.com>
2025-04-18 21:14:12 +0600

    Show the issues

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -12,7 +12,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas"; // Importing the validation schema
-import { set, z } from "zod";
+import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 

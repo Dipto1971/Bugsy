7083c5a2fa7f06330e7429d808611a883a8fe83a
Mahir Faysal Dipto <86009555+Dipto1971@users.noreply.github.com>
2025-04-25 12:35:46 +0600

    restructure NavBar component layout and improve styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,7 @@ import { AiFillBug } from "react-icons/ai";
 import { usePathname } from "next/navigation";
 import c_Names from "classnames";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 // In the classnaame object,
 // we can pass the classnames we want to render as keys and the condition as values.
 
@@ -19,40 +19,48 @@ const NavBar = () => {
   const links = [
     { href: "/", label: "Dashboard" },
     { href: "/issues/list", label: "Issues" },
-    { href: "/contact", label: "Contact" },
   ];
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        {" "}
-        <AiFillBug />
-      </Link>
-
-      <ul className="flex space-x-4">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={c_Names({
-                "text-zinc-900": currentPath === link.href,
-                "text-zinc-500": currentPath !== link.href,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-              href={link.href}
-            >
-              {link.label}
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        {/* Container will keep every component in a container */}
+        <Flex justify="between">
+          {/* The available space is distributed among the children */}
+          <Flex align="center" gap="3">
+            {/* First Child of the Flex Component */}
+            <Link href="/">
+              <AiFillBug />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Sign Out</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Sign In</Link>
-        )}
-      </Box>
+
+            <ul className="flex space-x-4">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={c_Names({
+                      "text-zinc-900": currentPath === link.href,
+                      "text-zinc-500": currentPath !== link.href,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Flex>
+            {/* Second Child of the Flex Component */}
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout">Sign Out</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Sign In</Link>
+            )}
+          </Flex>
+        </Flex>
+      </Container>
     </nav>
   );
 };

d4dff1a4e4ea76183a4ae49d670ba5fe25561a7d
Mahir Faysal Dipto <86009555+Dipto1971@users.noreply.github.com>
2025-04-25 10:51:06 +0600

    Set up Google Provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,16 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-  providers: [],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!, // Exclamation mark lets TypeScript know that this value is available.
+    }),
+  ],
 });
 
 export { handler as GET, handler as POST };
+
+// [...nextauth]/route.ts is a catch-all segment that will match any route under /api/auth.
+// This means that if you have a route like /api/auth/signin, it will be handled by this file.

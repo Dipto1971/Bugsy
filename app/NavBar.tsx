e256af249b3e1a0d511f4fe8a583c076aeea23eb
Mahir Faysal Dipto <86009555+Dipto1971@users.noreply.github.com>
2025-04-25 15:16:38 +0600

    Refactor NavBar component: extract AuthStatus and NavLinks into separate functions for better readability and maintainability

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -17,16 +17,6 @@ import {
 // we can pass the classnames we want to render as keys and the condition as values.
 
 const NavBar = () => {
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-  // We need to convert this component to a Client-Side Component,
-  // Because the usePathname hook is not supported on the server. This is a Browser API.
-
-  const links = [
-    { href: "/", label: "Dashboard" },
-    { href: "/issues/list", label: "Issues" },
-  ];
-
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Container>
@@ -38,58 +28,80 @@ const NavBar = () => {
             <Link href="/">
               <AiFillBug />
             </Link>
-
-            <ul className="flex space-x-4">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={c_Names({
-                      "text-zinc-900": currentPath === link.href,
-                      "text-zinc-500": currentPath !== link.href,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Flex>
-            {/* Second Child of the Flex Component */}
-            {status === "authenticated" && (
-              // <Link href="/api/auth/signout">Sign Out</Link>
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                    referrerPolicy="no-referrer"
-                    // referrerPolicy is used to prevent the browser from sending the user's personal information to the server.
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Sign Out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Sign In</Link>
-            )}
-          </Flex>
+          {/* Second Child of the Flex Component */}
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
   );
 };
-
 export default NavBar;
+
+const AuthStatus = () => {
+  const { data: session, status } = useSession();
+  if (status === "loading") return null;
+
+  if (status === "unauthenticated") {
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Sign In
+      </Link>
+    );
+  }
+  return (
+    <Box>
+      {/* Second Child of the Flex Component */}
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+            // referrerPolicy is used to prevent the browser from sending the user's personal information to the server.
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Sign Out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+  // We need to convert this component to a Client-Side Component,
+  // Because the usePathname hook is not supported on the server. This is a Browser API.
+
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues/list", label: "Issues" },
+  ];
+  return (
+    <ul className="flex space-x-4">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={c_Names({
+              "nav-link": true,
+              "!text-zinc-900": currentPath === link.href,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
